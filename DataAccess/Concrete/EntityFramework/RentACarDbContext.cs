6fc864befa3645c535a6788f3106ac2c0c972451
MyavKat <77459135+myavkat@users.noreply.github.com>
2022-06-28 15:37:07 +0300

    Connection String Enable SSL Mode

--- DataAccess/Concrete/EntityFramework/RentACarDbContext.cs
+++ DataAccess/Concrete/EntityFramework/RentACarDbContext.cs
@@ -11,7 +11,7 @@ namespace DataAccess.Concrete.EntityFramework
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseNpgsql(@"Server=ec2-3-248-121-12.eu-west-1.compute.amazonaws.com;Port=5432;User Id=xnhwrcdmqetigk;Password=5291b6b943e406991479239ff4bba5f6758efb4e72e11947732e4fab75ebf5f5;Database=d3girv9tp2hqks");
+            optionsBuilder.UseNpgsql(@"User ID=xnhwrcdmqetigk;Password=5291b6b943e406991479239ff4bba5f6758efb4e72e11947732e4fab75ebf5f5;Server=ec2-3-248-121-12.eu-west-1.compute.amazonaws.com;Port=5432;Database=d3girv9tp2hqks;Integrated Security=true;Pooling=true;SSL Mode=Require;Trust Server Certificate=true;");
         }
         public DbSet<Brand> Brands { get; set; }
         public DbSet<Car> Cars { get; set; }
