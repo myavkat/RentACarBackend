b10f25ea4e6e4df1a58982d08e2f37d683e62698
Mehmet Yavuz Katrancı <77459135+myavkat@users.noreply.github.com>
2022-07-07 00:22:23 +0300

    Delete DatabaseTest directory

--- DatabaseTest/dbo/Tables/Brands.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[Brands] (
-    [BrandId]   INT  NOT NULL,
-    [BrandName] TEXT NULL,
-    PRIMARY KEY CLUSTERED ([BrandId] ASC)
-);
-
--- DatabaseTest/dbo/Tables/Colors.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[Colors] (
-    [ColorId]   INT  NOT NULL,
-    [ColorName] TEXT NULL,
-    PRIMARY KEY CLUSTERED ([ColorId] ASC)
-);
-
